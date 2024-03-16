0cca78b1f7d25c409991655b2758238d29c0abf6
Lalit Malviya <gehlotlalit786@gmail.com>
2024-03-16 08:50:47 +0530

    SearchInput Complete

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -7,7 +7,14 @@ import MicIcon from "../assets/mic.svg";
 import ImageIcon from "../assets/image.svg";
 
 const SearchInput = () => {
-    const [searchQuery, setSearchQuery] = useState(""); 
+    const {query } = useParams();
+    const [searchQuery, setSearchQuery] = useState(query || "");
+    const navigate = useNavigate();
+    const searchQueryHandler = (event) => {
+        if(event.key === "Enter" && searchQuery.length > 0){
+             navigate(`${searchQuery}/${1}`);
+        }
+    };
     return (
         <div id="searchBox" 
         className="h-[46px] w-full md:w-[584px] flex items-center gap-3 px-4 border border-[#dfe1e5] rounded-3xl hover:bg-white hover:shadow-c hover:border-0 focus-within:shadow-c focus-within:border-0">
@@ -15,7 +22,7 @@ const SearchInput = () => {
             <input 
                 type="text" 
                 onChange={(e)=> setSearchQuery(e.target.value)}
-                onKeyUp={(e)=> console.log(e.target.value)}
+                onKeyUp={searchQueryHandler}
                 value={searchQuery} 
                 autoFocus 
                 className="grow outline-0 text-black/[0.87]"/>
